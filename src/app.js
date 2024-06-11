8fd3673edce6904b6fa50b2311f93452a1e6be52
Yash-Zanzarukiya <zanzarukiyayash2711@gmail.com>
2024-06-12 02:01:43 +0530

    update app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://play-tube-frontend-by-yashpz.vercel.app/*",
+      "https://play-tube-frontend-by-yashpz.vercel.app",
       "https://play-tube-by-yashpz.vercel.app",
     ],
     credentials: true,
