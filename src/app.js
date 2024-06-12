609c59f245e70d1d7ba6aca3c17c8808e6d1fae4
Yash-Zanzarukiya <zanzarukiyayash2711@gmail.com>
2024-06-12 21:38:18 +0530

    build(deps): update package-lock.json, package.json and app.js

--- src/app.js
+++ src/app.js
@@ -7,14 +7,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "*",
-      "https://play-tube-frontend-by-yashpz.vercel.app",
-      "http://localhost:5173",
-    ],
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
+// origin: process.env.CORS_ORIGIN,
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
