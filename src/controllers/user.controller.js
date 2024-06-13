09abda03995f8233b5cb012632fe575d6f8988be
Yash-Zanzarukiya <zanzarukiyayash2711@gmail.com>
2024-06-13 13:57:45 +0530

    cookie options modified

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -157,7 +157,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const cookieOptions = {
     httpOnly: true,
     secure: true,
-    sameSite: "none",
+    sameSite: "None",
   };
 
   return res
@@ -195,7 +195,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   const cookieOptions = {
     httpOnly: true,
     secure: true,
-    sameSite: "none",
+    sameSite: "None",
   };
   return res
     .status(200)
@@ -241,7 +241,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     const cookieOptions = {
       httpOnly: true,
       secure: true,
-      sameSite: "none",
+      sameSite: "None",
     };
 
     return res
