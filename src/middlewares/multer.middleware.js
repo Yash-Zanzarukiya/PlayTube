eb87ce3b54ead59ea762e864d9ee5b3f4483c23e
Yash-Zanzarukiya <zanzarukiyayash2711@gmail.com>
2024-06-13 20:34:42 +0530

    update multer.middleware.js

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,6 +1,6 @@
 import multer from "multer";
 
-const localTempPath = "public/temp";
+const localTempPath = "./public/temp";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
