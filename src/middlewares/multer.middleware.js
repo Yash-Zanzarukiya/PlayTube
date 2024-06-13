3108200f5739cd24a42e8cf17878db4ccdd261b6
Yash-Zanzarukiya <zanzarukiyayash2711@gmail.com>
2024-06-13 20:36:21 +0530

    update multer.middleware.js

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,6 +1,6 @@
 import multer from "multer";
 
-const localTempPath = "./public/temp";
+const localTempPath = "./public/temp/";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -9,9 +9,6 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
-    req.on("close", () => {
-      console.log("Multer request aborted...");
-    });
   },
 });
 
