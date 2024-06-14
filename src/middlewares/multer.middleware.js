8546d12ad5dc14ed4d4074acefd6399d7e3a6ca9
Yash-Zanzarukiya <zanzarukiyayash2711@gmail.com>
2024-06-14 08:17:48 +0530

    Size Increased for payload

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,20 +1,6 @@
 import multer from "multer";
-import fs from "fs";
-
-const localTempPath = "../../public/";
 
 const storage = multer.diskStorage({
-  // destination: function (req, file, cb) {
-  //   console.log("file: ", file);
-  //   console.log("LocalPath: ", localTempPath);
-
-  //   if (!fs.existsSync(localTempPath))
-  //     console.log(`Directory ${localTempPath} still does not exist.`);
-  //   else
-  //     console.log(`Directory ${localTempPath} exist.`);
-
-  //   cb(null, localTempPath);
-  // },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
     console.log("file.originalname: ", file.originalname);
