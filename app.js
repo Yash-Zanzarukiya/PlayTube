c4a626349bb4af10b6624847c8ce271e579629c0
Yash-Zanzarukiya <zanzarukiyayash2711@gmail.com>
2024-06-15 20:51:26 +0530

    limit updated for body

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(
 );
 // origin: process.env.CORS_ORIGIN,
 
-app.use(express.json({ limit: "16kb" }));
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
