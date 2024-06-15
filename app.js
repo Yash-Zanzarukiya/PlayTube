94decc95cd4c5bc8c920f66913f688e1182abf5e
Yash-Zanzarukiya <zanzarukiyayash2711@gmail.com>
2024-06-15 20:57:37 +0530

    updated req body size limit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(
 // origin: process.env.CORS_ORIGIN,
 
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(morgan("dev"));
